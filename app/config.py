d6845dc235085c558ede6dec3588ca0a22421e37
idx1008 <idx1008@gmail.com>
2019-03-04 18:48:34 +0000

    Refactoring

--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Gardens grows Sox and Candles'
     REPOSITORY = 'REPOSITORY.db'
     USER_AGENT = 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36'
-    APP_NAME = os.environ.get('RR_NAME') or 'Unknown'
+    APPNAME = os.environ.get('RR_NAME') or 'Unknown'
