91e175e221b373a6d98840b5d8b525e3ea86ba03
reddogma <id247528@gmail.com>
2019-03-04 23:38:52 +0000

    a

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 from app.config import Config
 
-app = Flask(__name__, template_folder='../templates', static_url_path='../static')
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config.from_object(Config)
 
 login = LoginManager(app)
