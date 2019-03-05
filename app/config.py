3d32811c4234cde53bf2d62c5d4e8e9d3b614bdf
reddogma <id247528@gmail.com>
2019-03-05 20:52:14 +0000

    a

--- app/config.py
+++ app/config.py
@@ -8,5 +8,5 @@ class Config(object):
     USER_AGENT = 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36'
     APPNAME = os.environ.get('RR_NAME') or 'Unknown'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'REPOSITORY.db')
+        'sqlite:///' + os.path.join(basedir, '../REPOSITORY.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
