e3eaee2d65a537206e62c7abeac89e39fe5505c7
reddogma <id247528@gmail.com>
2019-03-05 16:38:41 +0000

    fixing

--- app/models.py
+++ app/models.py
@@ -6,10 +6,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(UserMixin, flaskdb.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    id = flaskdb.Column(db.Integer, primary_key=True)
+    username = flaskdb.Column(db.String(64), index=True, unique=True)
+    email = flaskdb.Column(db.String(120), index=True, unique=True)
+    password_hash = flaskdb.Column(db.String(128))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
