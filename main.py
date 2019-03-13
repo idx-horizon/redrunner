fa043a5ffbb9a2e7c564085c157a4f3f7bf8da8b
reddogma <id247528@gmail.com>
2019-03-13 22:11:43 +0000

    a

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ from app.models import User
 @app.context_processor
 def utility_processor():
 	def hr():
-		if current_user.is_authenicated:
+		if current_user.is_authenticated:
 			return HOME_RUN + current_user.username
 		else:
 			return 'not logged in'
