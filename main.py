963689059216e19c286802a2f9623045dd0baa8a
reddogma <id247528@gmail.com>
2019-03-18 17:55:23 +0000

    a

--- main.py
+++ main.py
@@ -98,7 +98,8 @@ def logout():
 
 @app.route('/map')
 def gmap():
-	return render_template('map.html')
+	api_key = open.read('resources/gmap.key').read()[:-1]
+	return render_template('map.html', api_key=api_key)
 	
 @app.route('/home/')
 @app.route('/home/<name>')
