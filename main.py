5e7927af1e99c1b03d82c30cd2c6e3d81b76544a
reddogma <id247528@gmail.com>
2019-03-26 00:02:33 +0000

    a

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def gmap():
 	#centre = '{lat: 51.386539, lng: 0.022874}'
 	#centre = [51.410992, -0.335791]
 	h = current_user.home_run if current_user.is_authenticated else 'Bromley'
-	center = list(geo.get_coordinates(h,None))
+	centre = list(geo.get_coordinates(h,None))
 	
 	data = geo.closest_runs(h, top=10)
 	markers = [list(d.values()) for d in data]
