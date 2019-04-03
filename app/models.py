796a2b6ada4efe300c47d5282ae278c94c9182e3
idx1008 <idx1008@gmail.com>
2019-04-03 18:03:55 +0100

    refactoring

--- app/models.py
+++ app/models.py
@@ -4,9 +4,6 @@ from app import flaskdb, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-#class Apple(flaskdb.Model):
-#    xid     = flaskdb.Column(flaskdb.Integer, primary_key=True)
-#    xname   = flaskdb.Column(flaskdb.String(64), index=True, unique=True)
 
 class RunnerLink(flaskdb.Model):
     id = flaskdb.Column(flaskdb.Integer, primary_key=True)
