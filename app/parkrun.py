a3ab578196f4af615182541cc051ec99bce1a444
reddogma <id247528@gmail.com>
2019-04-05 16:22:44 +0100

    a

--- app/parkrun.py
+++ app/parkrun.py
@@ -69,7 +69,8 @@ def run(runner, LOCAL_DATA, mydb=None):
 	link ='http://www.parkrun.org.uk/results/athleteeventresultshistory/?athleteNumber=' + runner + '&eventNumber=0'
 
 	headers  =  {
-		'User-Agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36'
+		'User-Agent': app.config['USER_AGENT']
+#		'User-Agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36'
 		}	
 		
 	session = requests.Session()
