75d874017a7865f13a4f14766a37fc7a0bb2fb8d
idx <idx1008@gmail.com>
2019-12-02 17:30:31 +0000

    Update deployment-tasks.sh

--- deployment-tasks.sh
+++ deployment-tasks.sh
@@ -2,6 +2,6 @@ echo Copying master repository and keys
 
 ls
 
-docker cp gmap.key red:/redrunner/resources/gmap.key
+# docker cp gmap.key red:/redrunner/resources/gmap.key
 
 docker cp masterREPOSITORY.db red:/redrunner/REPOSITORY.db
